bd537400c22e54019593820c7cbcd91e8621dd8f
Yaroslav Markivskyi <129275754+YaroslavMarkivskyi@users.noreply.github.com>
2024-07-11 13:48:31 +0300

    Added get cars method

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from fastapi import FastAPI
+from typing import Optional, List, Dict
+
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body
+
+from database import cars
+from models import Car
 
 
 app = FastAPI()
+
+
+@app.get("/cars", response_model=List[Dict[str, Car]])
+def get_cars(number: Optional[str] = Query("10", max_length=3)):
+    response = []
+    for id, car in list(cars.items())[:int(number)]:
+        to_add = {}
+        to_add[id] = car
+        response.append(to_add)
+    return response
+
+
+
+
