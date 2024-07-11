ef3c7173c6cba7d946692150aa5aceede3a18183
Yaroslav Markivskyi <129275754+YaroslavMarkivskyi@users.noreply.github.com>
2024-07-11 13:49:53 +0300

    Added feature createt cars

--- main.py
+++ main.py
@@ -25,3 +25,17 @@ def get_car_by_id(car_id: int = Path(...,ge=0, lt=1000)):
     if not car:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Could not find car by ID.")
     return car
+
+
+@app.post("/cars", status_code = status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+    if len(cars) < 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No cars to add.")
+    min_id = len(cars.values()) + min_id
+    for car in body_cars:
+        while cars.get(min_id):
+            min_id += 1
+        cars[min_id] = car
+        min_id += 1
+
+
